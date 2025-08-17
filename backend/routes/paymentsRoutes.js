47ee5732a283c9c589d5bd18e9a3ff9a6a06e6da
alielabdo <142112307+alielabdo@users.noreply.github.com>
2025-08-17 14:03:07 +0300

    Update paymentsRoutes.js

--- backend/routes/paymentsRoutes.js
+++ backend/routes/paymentsRoutes.js
@@ -5,5 +5,5 @@ const { protect} = require('../middleWares/authMiddleware');
 const allowRoles = require('../middleWares/roleMiddleware');
 
 router.get('/', protect, allowRoles('admin'), getPayments);
-router.post('/',protect,allowRoles('admin'),createPayment);
+router.post('/',protect, allowRoles('admin'), createPayment);
 module.exports = router;
